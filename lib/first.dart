f79d6caf4000bd20d8e5ad6ec0b2c22dc0101788
kimts2627 <kimtstststs@gmail.com>
2021-04-29 22:15:46 +0900

    add two pages

---
+++ lib/first.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FirstPage'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            RaisedButton(
+              child: Text('back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
